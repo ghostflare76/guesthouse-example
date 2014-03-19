5aab983e95ba7b03d52a127aa495a990101ea9f9
cowboy76 <cowboy76@ncsoft.com>
2014-03-19 11:22:43 +0900

    Logo modify

--- src/main/java/com/cowboy76/batch/config/AppConfig.java
+++ src/main/java/com/cowboy76/batch/config/AppConfig.java
@@ -12,16 +12,16 @@ public class AppConfig {
 	public AppConfig() {
 		StringBuffer sbf = new StringBuffer();
 
-		sbf.append("                              _                       _                _             _     			\n");
-		sbf.append("                             (_)                     | |              | |          | |    			\n");
+		sbf.append("                               _                      _                _             _     			\n");
+		sbf.append("                              (_)                    | |              | |          | |    			\n");
 		sbf.append("        ___  _ __   _ __  _  _ __    __ _   | |__    __ _ | |_   ___ | |__  			\n");
-		sbf.append("       / __|| '_ \\ | '__|| || '_ \\  /  _` |   | '_ \\  /  _` ||  __| / __|| '_  \\ 	\n");
-		sbf.append("       \\__ \\| |_) || |   | || | | || (_| |   | |_) || (_| || |_ | (__ | |  | |		\n");
-		sbf.append("       |___/| .__/|_|  |_||_| |_| \\__,|   |_.__/ \\__,_| \\__|\\___||_| |_|	\n");
-		sbf.append("            | |                           __/  |                                    					\n");
-		sbf.append("            |_|                         |___/                                     					\n");
-		sbf.append("                                                                                          				 \n");
-		sbf.append(":: Welcome to the cowboy76 project________________________________        \n");
+		sbf.append("       / __|| '_  \\ | '__|| || '_ \\  /  _` |  | '_  \\ /  _` ||  __|/ __|| '_  \\ 	\n");
+		sbf.append("       \\__ \\| |_) || |    | || | | || (_| |  | |_) || (_| || |_ | (__ | | | |		\n");
+		sbf.append("       |___/| .__/|_|   |_||_||_| \\__, |  |_.__/ \\__,_| \\__|\\___||_| |_|	\n");
+		sbf.append("              | |                          __/  |                                    				\n");
+		sbf.append("              |_|                         |___/                                     				\n");
+		sbf.append("                                                                                          				\n");
+		sbf.append(":: Welcome to the cowboy76 project________________________________   \n");
 		System.out.println(sbf.toString());
 
 	}
